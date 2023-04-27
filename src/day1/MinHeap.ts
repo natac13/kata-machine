a7b7848ed4a4d3add10a8263b327abc6d0cb4f8b
Sean Campbell <sean.campbell13@gmail.com>
2023-04-27 06:57:49 -0400

    stuff

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -19,6 +19,9 @@ export default class MinHeap {
         }
 
         const out = this.data[0];
+        // we set the length to be one less than the current length
+        // as we are going to remove the root node
+        // and heapify down uses the length to determine when to stop
         this.length--;
 
         if (this.length === 0) {
@@ -26,43 +29,52 @@ export default class MinHeap {
             return out;
         }
 
+        // take the last element in the array and put it in the root
+        // then heapify down, which will put the new root in the correct position
+        // which self balances the heap
         this.data[0] = this.data[this.length];
         this.heapifyDown(0);
         return out;
     }
 
+    // get a node's parent index in the array
     private parentIdx(idx: number): number {
         return Math.floor((idx - 1) / 2);
     }
 
+    // get a node's left child index in the array
     private leftChildIdx(idx: number): number {
         return 2 * idx + 1;
     }
 
+    // get a node's right child index in the array
     private rightChildIdx(idx: number): number {
         return 2 * idx + 2;
     }
 
     private swap(idx1: number, idx2: number): void {
-        const temp = this.data[idx1];
-        this.data[idx1] = this.data[idx2];
-        this.data[idx2] = temp;
+        // const temp = this.data[idx1];
+        // this.data[idx1] = this.data[idx2];
+        // this.data[idx2] = temp;
+        [this.data[idx1], this.data[idx2]] = [this.data[idx2], this.data[idx1]];
     }
 
     private heapifyUp(idx: number): void {
-        if (idx === 0) {
+        if (idx <= 0) {
             return;
         }
 
-        const pIdx = this.parentIdx(idx);
-        const pValue = this.data[pIdx];
+        const parentIdx = this.parentIdx(idx);
+        const parentValue = this.data[parentIdx];
         const value = this.data[idx];
 
-        if (pValue > value) {
-            // this.data[pIdx] = value;
-            // this.data[idx] = pValue;
-            this.swap(pIdx, idx);
-            this.heapifyUp(pIdx);
+        // as this is a min heap,
+        // when the parent is larger than the current value,
+        // we swap the parent and the current value in the array
+        // and continue to heapify up until the base case is reached
+        if (parentValue > value) {
+            this.swap(parentIdx, idx);
+            this.heapifyUp(parentIdx);
         }
     }
 
@@ -74,6 +86,10 @@ export default class MinHeap {
         const lIdx = this.leftChildIdx(idx);
         const rIdx = this.rightChildIdx(idx);
 
+        // since we alway fill in the heap from left to right,
+        // if the left child index is larger than the length of the array,
+        // it means that the current node has no children
+        // and we can return
         if (lIdx >= this.length) {
             return;
         }
@@ -84,15 +100,21 @@ export default class MinHeap {
 
         const minValue = Math.min(lValue, rValue);
 
+        // if the current value is larger than the min value of its children,
+        // we swap the current value with the min value
         if (minValue < value) {
+            // if the min value is the left child,
             if (minValue === lValue) {
                 this.swap(lIdx, idx);
                 this.heapifyDown(lIdx);
             } else {
+                // if the min value is the right child,
                 this.swap(rIdx, idx);
                 this.heapifyDown(rIdx);
             }
         } else {
+            // if the current value is smaller than the min value of its children,
+            // we can return
             return;
         }
 
